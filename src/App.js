5978037e035a70f7659b5f44a9cd208391fb2347
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-08-07 05:02:36 +0530

    nav css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
+import "./styles/app.css"
 const App = ()=> {
   return  (
 <Router>
