24c2daa24d5a943c0aa55a11a6ca045cd7c27ddc
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-08-07 04:10:52 +0530

    add about skills project contact navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Nav from"./nav/Nav.js";
-
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 const App = ()=> {
   return  (
 <Router>
   <Nav />
   <Routes>
-    <Route path="/"/>
+    <Route path="/" element={About} />
+    <Route path="/skills" element={Skills} />
+    <Route path="/projects" element={Projects} />
+    <Route path="/contact" element={Contact} />
   </Routes>
 </Router>
 );
---
+++ src/skills/Skills.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
