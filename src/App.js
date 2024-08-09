87eaba8f58660af990f6111353becf5486fa16b4
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-08-10 03:02:18 +0530

    update name

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import "./styles/app.css"
 import Background from"./background/Background.js";
+import PlayerStats from "./playerStats/PlayerStats.js";
 const App = ()=> {
   return  (
 <Router>
   <Nav />
   <Background/>
   <Routes>
-    <Route path="/" element={About} />
-    <Route path="/skills" element={Skills} />
-    <Route path="/projects" element={Projects} />
-    <Route path="/contact" element={Contact} />
+    <Route path="/" element={<About/>} />
+    <Route path="/skills" element={<Skills/>} />
+    <Route path="/projects" element={<Projects/>} />
+    <Route path="/contact" element={<Contact/>} />
   </Routes>
+<PlayerStats/>
 </Router>
 );
 };
---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,17 @@
+import react from "react"
+import "../styles/playerStats.css"
+
+
+const PlayerStats= () =>{
+  return(
+  <div id="playerStats">
+        <h1>WASIF AlI LEVEL 31</h1>
+    <div id="playerStats_lines">
+        <span id="player_lines_thick"></span>
+        <span id="player_lines_thin"></span>
+    </div>
+    </div>
+  );
+} ;
+
+export default PlayerStats;
