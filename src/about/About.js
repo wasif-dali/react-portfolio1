241d57a7a92e0dd7f26871bc5d27bfc9a94e8c9c
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-08-12 20:46:21 +0530

    error in avatar

--- src/about/About.js
+++ src/about/About.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Avatar from "../avatar/Avatar.js";
+import AboutMenu from "./AboutMenu.js";
 
-export default class about extends Component {
+export default class About extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <Avatar page="about" />
+        <AboutMenu />
+      </>
+    );
   }
 }
