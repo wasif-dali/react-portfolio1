37cbb19c53b2e746cf00b090bb70e1b34a2b02d4
Wasif Ali <91631681+wasif-dali@users.noreply.github.com>
2024-08-20 22:26:38 +0530

    final baout

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -1,71 +1,77 @@
 const subheadingsData = {
-    1: [
-      {
-        title: "About Me",
-        content: (
-          <>
-            <p>
-            I am a skilled web developer with expertise in both front-end and back-end technologies. My proficiency spans across a wide range of languages and frameworks, including HTML, CSS, JavaScript, TypeScript, Node.js, Angular.js, Next.js, and React Native. I also have extensive experience with databases like MongoDB, MySQL, and SQL, and I’m adept at using Python with Django and Flask for backend development.
-            </p>
-          </>
-        ),
-      },
-    ],
-    2: [
-      {
-        title: "2022 Masters Degree",
-        content: (
-          <>
-            <p>
-          Master degree in computer Science "MCA"master of computer application
-            </p>
-          </>
-        ),
-      },
-      {
-        title: "2020 Bachelors Degree",
-        content: (
-          <>
-            <p>
-              
-            </p>
-          </>
-        ),
-      },
-    ],
-    3: [
-      {
-        title: "2022 Backend Developer ",
-        content: (
-          <>
-            <p>
-              
-            </p>
-          </>
-        ),
-      },
-      {
-        title: "2020 Graduate ",
-        content: (
-          <>
-            <p>
-              graduate in computer science 
-            </p>
-          </>
-        ),
-      },
-      {
-        title: "2017 join ",
-        content: (
-          <>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </>
-        ),
-      },
-    ],
-  };
-  
-  export default subheadingsData;
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I specialize in full-stack development, focusing on creating
+            efficient and scalable applications. I oversee both front-end and
+            back-end development to create seamless, high-performance web
+            solutions.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2022 Masters Degree",
+      content: (
+        <>
+          <p>
+            Master degree in computer Science "MCA"master of computer
+            application
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            Bachelors degree in computer Science "BCA" Bachelors of computer
+            application
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2021 Backend Developer ",
+      content: (
+        <>
+          <p></p>
+        </>
+      ),
+    },
+    {
+      title: "2020 Graduate ",
+      content: (
+        <>
+          <p>graduate in computer science</p>
+        </>
+      ),
+    },
+    {
+      title: "2022 Backend Developer  ",
+      content: (
+        <>
+          <p>
+            Learn backend development with expertise in JavaScript, Node.js,
+            MongoDB, and Express.js. I specialize in creating robust, scalable
+            server-side applications, focusing on efficiency and performance. My
+            skill set includes designing and managing databases, developing
+            RESTful APIs, and ensuring seamless integration between front-end
+            and back-end systems. I am committed to leveraging my knowledge to
+            build reliable and high-performing backend solutions.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
